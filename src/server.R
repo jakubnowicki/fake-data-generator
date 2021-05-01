1bfd23df39fa8d60676ca9656eac35465edc5c34
Jakub Nowicki <kuba@appsilon.com>
2021-05-01 15:03:48 +0200

    Basic structure.

--- src/server.R
+++ src/server.R
@@ -1,3 +1,13 @@
 server <- function(input, output, session) {
-
+  is_download_panel_open <- reactiveVal(FALSE)
+  output$download_panel <- renderReact({
+    Panel(
+      headerText = "Download data",
+      isOpen = is_download_panel_open(),
+       "Download buttons and stuff",
+      onDismiss = JS("function() { Shiny.setInputValue('hide_download_panel', Math.random()); }")
+    )
+  })
+  observeEvent(input$show_download_panel, is_download_panel_open(TRUE))
+  observeEvent(input$hide_download_panel, is_download_panel_open(FALSE))
 }
