79e3d83a0e8051283ed3e1f9cf4a845210cd8475
Jakub Nowicki <kuba@appsilon.com>
2021-05-01 18:27:51 +0200

    Add refresh button.

--- src/modules/data_preview.R
+++ src/modules/data_preview.R
@@ -7,13 +7,27 @@ export("server")
 ui <- function(id) {
   ns <- NS(id)
 
-  reactableOutput(ns("preview_data"))
+  div(
+    shiny.fluent::DefaultButton.shinyInput(ns("refresh_data"), text = "Refresh data"),
+    shiny.fluent::Separator("Fake data", alignContent = "center"),
+    reactableOutput(ns("preview_data"))
+  )
 }
 
 server <- function(id) {
   moduleServer(id, {
     function(input, output, session) {
+      trigger <- reactiveVal(0)
+
+      observeEvent(input$refresh_data, {
+        session$userData$fake_data$get_all_data(refresh = TRUE)
+
+        trigger_value <- trigger() + 1
+        trigger(trigger_value)
+      })
+
       output$preview_data <- renderReactable({
+        force(trigger())
         reactable(session$userData$fake_data$get_all_data()[[1]])
       })
     }
