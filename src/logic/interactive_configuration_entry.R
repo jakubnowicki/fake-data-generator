9df843a936f65c2142f3829e60e5516e1bcfc5f1
Jakub Nowicki <kuba@appsilon.com>
2021-05-13 18:09:36 +0200

    Some wip stuff...

--- src/logic/interactive_configuration_entry.R
+++ src/logic/interactive_configuration_entry.R
@@ -3,6 +3,33 @@ import("shiny")
 export("ui")
 export("server")
 
+create_inputs <- function(type, configuration, ns) {
+  type_configuration <- configuration[[type]]
+
+  span(
+    purrr::imap(
+      type_configuration[[-1]],
+      ~ create_input(
+        name = .y,
+        type = .x$input,
+        ns = ns,
+        value = .x$default
+      )
+    )
+  )
+}
+
+create_input <- function(name, type, ns, value) {
+  do.call(
+    type,
+    list(
+      inputId = ns(name),
+      label = name,
+      value = value
+    )
+  )
+}
+
 ui <- function(id, row_id) {
   ns <- NS(id)
 
