3345f94aff551d7246247c6a4771ac8e2ea0904c
Jakub Nowicki <kuba@appsilon.com>
2021-05-14 11:20:52 +0200

    Display stuff...

--- src/ui.R
+++ src/ui.R
@@ -5,21 +5,22 @@ fluidPage(
   div(
     class = "grid-container",
     div(
-      "Preview",
       class = "preview panel",
       data_preview$ui("data_preview")
     ),
     div(
-      "Editor",
       class = "editor panel",
       yaml_editor$ui("yaml_editor", autocompleter)
+    ),
+    div(
+      class = "header",
+      h1("Fake Data Generator"),
+      div(class = "menu-buttons",
+        DefaultButton.shinyInput("show_about_panel", text = "About"),
+        DefaultButton.shinyInput("show_help_panel", text = "Help"),
+        DefaultButton.shinyInput("show_download_panel", text = "Download")
+      )
     )
-  ),
-  div(
-    class = "download-panel-button",
-    DefaultButton.shinyInput("show_about_panel", text = "About"),
-    DefaultButton.shinyInput("show_help_panel", text = "Help"),
-    DefaultButton.shinyInput("show_download_panel", text = "Download")
   ),
   reactOutput("download_panel"),
   reactOutput("about_panel"),
